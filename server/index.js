2af4d61e200356ccce0b23cdfba62775aea36823
Apratim Dutta <2021itb089.apratim@students.iiests.ac.in>
2024-06-29 12:06:28 +0530

    changes 2

--- server/index.js
+++ server/index.js
@@ -4,30 +4,6 @@ const app = express();
 import connectToDb from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
-<<<<<<< Updated upstream
-
-=======
-connectToDb();
-// // connect to db
-// connectToDb();
-
-// //require routes
-// const userRouter = require('./routes/userRouter');
-// const otpRouter = require('./routes/otpRoutes');
-// const placesRouter =require('./routes/placesRoutes');
-// // requiring middlewares
-// const errorMiddleware = require('./middleware/Error');
-
-// initialize express
-// app.use(express.json({ limit: '20mb' }));
-// app.use(cookieParser());
-
-// using routers
-// app.use('/api/user', userRouter);
-// app.use('/api/otp', otpRouter);
-// app.use('/api/places',placesRouter);
-//send hello on /
->>>>>>> Stashed changes
 app.get('/', (req, res) => {
   res.send('Welcome to Test Secure api services');
 });
